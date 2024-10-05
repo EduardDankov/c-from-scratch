41be7fa8627275e265bb373a3c0d8efe4ef05e3b
Eduard Dankov <eduard.dankov@gmail.com>
2024-10-05 19:58:24 +0300

    [C-3] Move print functions to output library (#5)

--- lib/string.h
+++ lib/string.h
@@ -4,13 +4,6 @@
 #include <unistd.h>
 
 int str_size(const char * str);
-
-void print_c(const char c);
-void print_ln();
-void print_str_sz(const char * str, const int size);
-void print_str_sz_ln(const char * str, const int size);
 char * str_copy(char * dest, const char * src);
-void print_str(const char * str);
-void print_str_ln(const char * str);
 
 #endif // STRING_H
